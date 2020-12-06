f9cc87a0ca50e99ad5b2ac860c0b048767836387
Krzysztof Heigel <kfheigel@gmail.com>
2020-12-06 19:23:50 +0100

    Added Debug bundle

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(LoggerInterface $logger): Response
     {
+        $logger->info('IndexController started working');
         dump($this);
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
